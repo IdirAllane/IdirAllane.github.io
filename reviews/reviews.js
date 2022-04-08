67b4b949bf2c8e25b938ce3bef8416685f3e4937
Idir Allane <idir.allane@icloud.com>
2022-04-09 00:03:20 +0200

    changed reviews data

--- reviews/reviews.js
+++ reviews/reviews.js
@@ -4,7 +4,7 @@ export const reviews = [
         name: "John Wick",
         job: "Web Developer",
         img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Reuni%C3%A3o_com_o_ator_norte-americano_Keanu_Reeves_%28cropped%29.jpg/1280px-Reuni%C3%A3o_com_o_ator_norte-americano_Keanu_Reeves_%28cropped%29.jpg",
-        review: "Selfies hexagon +1 mixtape man braid portland celiac godard crucifix synth lumbersexual fam seitan chartreuse skateboard. Art party vinyl food truck whatever la croix iPhone forage tbh, gentrify flannel crucifix slow-carb disrupt vape.",
+        review: "Selfies hexagon +1 mixtape man braid portland celiac godard crucifix synth lumbersexual fam seitan chartreuse skateboard. Art party vinyl food truck whatever la croix iPhone forage tbh, gentrify flannel crucifix slow vape.",
     },
     {
         id: 2,
@@ -18,13 +18,13 @@ export const reviews = [
         name: "James Bond",
         job: "AWS Developer",
         img: "https://image.gala.de/22315258/t/sM/v6/w960/r1.5/-/daniel-craig.jpg",
-        review: "Try-hard kombucha migas, viral keffiyeh mixtape butcher everyday carry chicharrones YOLO. Bespoke ramps selvage offal locavore palo santo XOXO poke glossier. Church-key fanny pack food truck craft beer selfies XOXO cronut.",
+        review: "Try-hard kombucha migas, viral keffiyeh mixtape butcher everyday carry chicharrones YOLO. Bespoke ramps selvage offal locavore palo santo XOXO poke glossier. Church-key fanny pack food truck XOXO cronut craft messenger.",
     },
     {
         id: 4,
         name: "Bruce Willis",
         job: "Web Developer",
         img: "https://image.stern.de/31745292/t/jL/v7/w1440/r1/-/bruce-willis-aphasie-reaktionen.jpg",
-        review: "Yuccie XOXO everyday carry pinterest hell of blog cred master cleanse hot chicken narwhal cray. Vexillologist semiotics irony lumbersexual bitters celiac before they sold out PBR&B.",
+        review: "Yuccie XOXO everyday carry pinterest hell of blog cred master cleanse hot chicken narwhal cray. Vexillologist semiotics irony lumbersexual bitters celiac before they sold out PBR&B -carb disrupt beer selfies crucifix.",
     },
 ];
