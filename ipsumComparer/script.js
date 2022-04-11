c0e85b80782fb777de1622220fc2d4bd7fe42033
Idir Allane <idir.allane@icloud.com>
2022-04-11 14:28:12 +0200

    removed console.logs

--- ipsumComparer/script.js
+++ ipsumComparer/script.js
@@ -4,7 +4,6 @@ ipsums.forEach((ipsum) => {
     const btn = ipsum.querySelector("button");
 
     btn.addEventListener("click", (e) => {
-        console.log("click");
         ipsums.forEach((otherIpsum) => {
             if (otherIpsum !== ipsum) {
                 otherIpsum.children[1].classList.remove("show");
